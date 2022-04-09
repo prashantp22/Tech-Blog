2664a968cdd9c524a5b768adb5ae5e1ec95dabe1
PrashantP <prashantpatel622@gmail.com>
2022-04-09 14:57:19 -0400

    create post,comment tables

--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,6 @@
 const seedUsers = require('./user-seeds');
-// const seedPosts = require('./post-seeds');
-// const seedComments = require('./comment-seeds');
+const seedPosts = require('./post-seeds');
+const seedComments = require('./comment-seeds');
 
 const sequelize = require('../config/connection');
 
@@ -10,11 +10,11 @@ const seedAll = async () => {
     await seedUsers();
     console.log('--------------');
   
-    // await seedPosts();
-    // console.log('--------------');
+    await seedPosts();
+    console.log('--------------');
   
-    // await seedComments();
-    // console.log('--------------');
+    await seedComments();
+    console.log('--------------');
 
     process.exit(0);
 };
